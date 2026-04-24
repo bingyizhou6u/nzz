50e9775a2b920ac3fae35d5f86c58c4f5a2a509e
ARen <bingyizhou6u@gmail.com>
2026-04-25 03:50:24 +0400

    test: expect pending cost applications in fifo reversal

--- tests/domain/fifoReversal.test.ts
+++ tests/domain/fifoReversal.test.ts
@@ -63,6 +63,7 @@ describe("planSafeFifoReversalEffects", () => {
         }
       ],
       pendingCostCreations: [],
+      pendingCostApplications: [],
       pendingCostUpdates: []
     });
   });
